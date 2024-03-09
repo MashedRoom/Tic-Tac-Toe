4a4fc1753248dfbd9e03d6b237bb79b0e32a2e28
JOJO <jovanisk321@gmail.com>
2024-03-09 22:17:59 +0700

    restartgame

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const cells = document.querySelectorAll(".cell")
+const turn = document.querySelector(".turn")
+const restartBtn = document.querySelector(".restartBtn")
 
 let gameBoard = {
     board : [
@@ -19,16 +21,17 @@ function player(name, shape){
     return {name, shape}
 }
 
-let moves = 1
+let moves = 0
 function playerMove(p1, p2, ycor, xcor, cellIndex){
     let shape
-    if(moves % 2 == 1){
+    if(moves % 2 == 0){
         shape = p1.shape
-        cells[cellIndex].textContent = shape 
+        turn.textContent = p2.shape + " turn"
     } else{
         shape = p2.shape
-        cells[cellIndex].textContent = shape
+        turn.textContent = p1.shape+ " turn"
     }
+    cells[cellIndex].textContent = shape 
     moves++
     gameBoard.setBoard(ycor, xcor, shape)
 }
@@ -45,36 +48,70 @@ function checkWinner(shape){
         if (gameBoard.board[i][0] == shape && gameBoard.board[i][1] == shape && gameBoard.board[i][2] == shape){
             gameOver = true
             console.log("WINNER" + shape)
+            turn.textContent = shape + " IS THE WINNER"
+            updateListener()
         }
     }
     for (let i = 0; i<3; i++){
         if (gameBoard.board[0][i] == shape && gameBoard.board[1][i] == shape && gameBoard.board[2][i] == shape){
             gameOver = true
-            console.log("WINNER IS " + shape)
+            turn.textContent = shape + " IS THE WINNER"
+            updateListener()
         }
     }
     if(gameBoard.board[1][1] ==shape && gameBoard.board[0][0] == shape && gameBoard.board[2][2]){
         gameOver= true
-        console.log("WINNER IS " + shape)
+        turn.textContent = shape + " IS THE WINNER"
+        updateListener()
     }
     if(gameBoard.board[1][1] ==shape && gameBoard.board[0][2] == shape && gameBoard.board[2][0]){
         gameOver= true
-        console.log("WINNER" + shape)
+        turn.textContent = shape + " IS THE WINNER"
+        updateListener()
     }
     if(moves == 9){
         gameOver= true
-        console.log("Draw")
+        turn.textContent = "DRAW"
+        updateListener()
     }
 }
 
-cells.forEach((cell)=>{
-    cell.addEventListener(("click"), ()=>{
-        let cellIndex = Array.from(cells).indexOf(cell)
-        let ycor = Math.floor(cellIndex/3)
-        let xcor = (cellIndex%3)
-        console.log(ycor, xcor)
-        playerMove(p1, p2, ycor, xcor, cellIndex)
-        checkWinner("x")
-        checkWinner("o")
+function cellClicked(event){
+    let cellIndex = Array.from(cells).indexOf(event.target)
+    let ycor = Math.floor(cellIndex/3)
+    let xcor = (cellIndex%3)
+    console.log(ycor, xcor)
+    playerMove(p1, p2, ycor, xcor, cellIndex)
+    checkWinner("x")
+    checkWinner("o")
+}
+
+function updateListener(){
+    if (!gameOver){
+        cells.forEach((cell)=>{
+            cell.addEventListener(("click"), cellClicked)
+        })
+    }
+    else{
+        cells.forEach((cell)=>{
+            cell.removeEventListener(("click"), cellClicked)
+        })
+    }
+}
+updateListener()
+
+function restartGame(){
+    gameOver = false
+    moves = 0
+    cells.forEach((cell)=>{
+        cell.textContent = ""
     })
-})
+    turn.textContent = "x turn"
+    gameBoard.board = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]]
+    updateListener()
+}
+
+restartBtn.addEventListener(("click"), restartGame)
